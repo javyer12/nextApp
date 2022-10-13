e1288713803bca3bfcbe2a874c7889e35eb2b442
javyer <70788090+javyer12@users.noreply.github.com>
2022-10-12 19:18:02 -0600

    next app

---
+++ Documents/proyects/nextjs/react-shop-admin/src/pages/_app.js
@@ -0,0 +1,8 @@
+// import "../styles/tailwind.css";
+import "../../node_modules/tailwindcss/tailwind.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
